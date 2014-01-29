d5868d207de7d102db12def5b4ce479187b13ef5
Ole Morten Haaland <ole.morten.haaland@nordicsemi.no>
2014-01-29 13:09:30 +0100

    Correct comments.

--- ble_lbs.h
+++ ble_lbs.h
@@ -12,23 +12,23 @@
 
 /** @file
  *
- * @defgroup ble_sdk_srv_bas Battery Service
+ * @defgroup ble_sdk_srv_bas LED Button Service
  * @{
  * @ingroup ble_sdk_srv
- * @brief Battery Service module.
+ * @brief LED Button Service module.
  *
- * @details This module implements the Battery Service with the Battery Level characteristic.
- *          During initialization it adds the Battery Service and Battery Level characteristic
+ * @details This module implements the LED Button Service with the Battery Level characteristic.
+ *          During initialization it adds the LED Button Service and Battery Level characteristic
  *          to the BLE stack database. Optionally it can also add a Report Reference descriptor
- *          to the Battery Level characteristic (used when including the Battery Service in
+ *          to the Battery Level characteristic (used when including the LED Button Service in
  *          the HID service).
  *
  *          If specified, the module will support notification of the Battery Level characteristic
  *          through the ble_lbs_battery_level_update() function.
- *          If an event handler is supplied by the application, the Battery Service will
- *          generate Battery Service events to the application.
+ *          If an event handler is supplied by the application, the LED Button Service will
+ *          generate LED Button Service events to the application.
  *
- * @note The application must propagate BLE stack events to the Battery Service module by calling
+ * @note The application must propagate BLE stack events to the LED Button Service module by calling
  *       ble_lbs_on_ble_evt() from the from the @ref ble_stack_handler callback.
  *
  * @note Attention! 
@@ -44,20 +44,18 @@
 #include "ble.h"
 #include "ble_srv_common.h"
 
+
 // Forward declaration of the ble_lbs_t type. 
 typedef struct ble_lbs_s ble_lbs_t;
 
-/**@brief Battery Service event handler type. */
 typedef void (*ble_lbs_led_write_handler_t) (ble_lbs_t * p_lbs, uint8_t new_state);
 
-/**@brief Battery Service init structure. This contains all options and data needed for
- *        initialization of the service.*/
 typedef struct
 {
     ble_lbs_led_write_handler_t led_write_handler;                    /**< Event handler to be called when LED characteristic is written. */
 } ble_lbs_init_t;
 
-/**@brief Battery Service structure. This contains various status information for the service. */
+/**@brief LED Button Service structure. This contains various status information for the service. */
 typedef struct ble_lbs_s
 {
     uint16_t                    service_handle;
@@ -68,9 +66,9 @@ typedef struct ble_lbs_s
     ble_lbs_led_write_handler_t led_write_handler;
 } ble_lbs_t;
 
-/**@brief Function for initializing the Battery Service.
+/**@brief Function for initializing the LED Button Service.
  *
- * @param[out]  p_lbs       Battery Service structure. This structure will have to be supplied by
+ * @param[out]  p_lbs       LED Button Service structure. This structure will have to be supplied by
  *                          the application. It will be initialized by this function, and will later
  *                          be used to identify this particular service instance.
  * @param[in]   p_lbs_init  Information needed to initialize the service.
@@ -81,14 +79,10 @@ uint32_t ble_lbs_init(ble_lbs_t * p_lbs, const ble_lbs_init_t * p_lbs_init);
 
 /**@brief Function for handling the Application's BLE Stack events.
  *
- * @details Handles all events from the BLE stack of interest to the Battery Service.
+ * @details Handles all events from the BLE stack of interest to the LED Button Service.
  *
- * @note For the requirements in the BAS specification to be fulfilled,
- *       ble_lbs_battery_level_update() must be called upon reconnection if the
- *       battery level has changed while the service has been disconnected from a bonded
- *       client.
  *
- * @param[in]   p_lbs      Battery Service structure.
+ * @param[in]   p_lbs      LED Button Service structure.
  * @param[in]   p_ble_evt  Event received from the BLE stack.
  */
 void ble_lbs_on_ble_evt(ble_lbs_t * p_lbs, ble_evt_t * p_ble_evt);

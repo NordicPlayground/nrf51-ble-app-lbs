d2297b9d00055e9643c58df6dcb89f99d3d30cd5
Ole Morten Haaland <ole.morten.haaland@nordicsemi.no>
2014-01-29 13:32:44 +0100

    Correct security mode for button.

--- ble_lbs.c
+++ ble_lbs.c
@@ -132,10 +132,8 @@ static uint32_t button_char_add(ble_lbs_t * p_lbs, const ble_lbs_init_t * p_lbs_
 
     memset(&cccd_md, 0, sizeof(cccd_md));
 
-    // According to BAS_SPEC_V10, the read operation on cccd should be possible without
-    // authentication.
     BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.read_perm);
-    cccd_md.write_perm = p_lbs_init->battery_level_char_attr_md.cccd_write_perm;
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.write_perm);
     cccd_md.vloc = BLE_GATTS_VLOC_STACK;
     
     memset(&char_md, 0, sizeof(char_md));
@@ -152,8 +150,8 @@ static uint32_t button_char_add(ble_lbs_t * p_lbs, const ble_lbs_init_t * p_lbs_
     
     memset(&attr_md, 0, sizeof(attr_md));
 
-    attr_md.read_perm  = p_lbs_init->battery_level_char_attr_md.read_perm;
-    attr_md.write_perm = p_lbs_init->battery_level_char_attr_md.write_perm;
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.read_perm);
+    BLE_GAP_CONN_SEC_MODE_SET_NO_ACCESS(&attr_md.write_perm);
     attr_md.vloc       = BLE_GATTS_VLOC_STACK;
     attr_md.rd_auth    = 0;
     attr_md.wr_auth    = 0;

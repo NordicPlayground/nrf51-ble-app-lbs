5322e9a1fdddea5ab4ddd9660fcde7b6e23e1732
Ole Morten Haaland <ole.morten.haaland@nordicsemi.no>
2014-02-20 11:06:26 +0100

    Add bond manager configuration

---
+++ ble_bondmngr_cfg.h
@@ -0,0 +1,19 @@
+/* Copyright (c) 2013 Nordic Semiconductor. All Rights Reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the license.txt file.
+ */
+
+#ifndef BLE_BONDMNGR_CFG_H__
+#define BLE_BONDMNGR_CFG_H__
+
+/**@brief Number of CCCDs used in the LED Button application. */
+#define BLE_BONDMNGR_CCCD_COUNT            1
+
+/**@brief Maximum number of bonded centrals. */
+#define BLE_BONDMNGR_MAX_BONDED_CENTRALS   7
+
+#endif // BLE_BONDMNGR_CFG_H__
+
+/** @} */
+/** @endcond */

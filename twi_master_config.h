9ff1361175d54c05ff84e80bae27cef921ea5c08
Ole Morten Haaland <ole.morten.haaland@nordicsemi.no>
2014-03-11 13:49:28 +0100

    Add twi config file

---
+++ twi_master_config.h
@@ -0,0 +1,13 @@
+/* Copyright (c) 2013 Nordic Semiconductor. All Rights Reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the license.txt file.
+ */
+
+#ifndef TWI_MASTER_CONFIG
+#define TWI_MASTER_CONFIG
+
+#define TWI_MASTER_CONFIG_CLOCK_PIN_NUMBER (24U)
+#define TWI_MASTER_CONFIG_DATA_PIN_NUMBER (25U)
+
+#endif
